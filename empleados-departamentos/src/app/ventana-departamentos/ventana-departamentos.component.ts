d592008b5e45ef6ff730be00797a3d3eef487671
sebasc49 <scardonas920@gmail.com>
2025-01-28 14:11:23 -0500

    Creacion Frontend

---
+++ empleados-departamentos/src/app/ventana-departamentos/ventana-departamentos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ventana-departamentos',
+  imports: [],
+  templateUrl: './ventana-departamentos.component.html',
+  styleUrl: './ventana-departamentos.component.css'
+})
+export class VentanaDepartamentosComponent {
+
+}
